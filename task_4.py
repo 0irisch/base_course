a95f27b82830fdee665c282944aafa54e02acde8
0irisch <isepeleva2045@gmail.com>
2025-10-24 14:25:12 +0000

    jhlk

---
+++ task_4.py
@@ -0,0 +1,8 @@
+a = [1, 5, 'Good', 'Bad']
+b = [9, 'Blue', 'Red', 11]
+a.append(11)
+print(a)
+print(a[2] + b[2])
+print(a[0] * b[0])
+print(a[1] ** b[3])
+print(a + b)
---
+++ task_6.py
@@ -0,0 +1,7 @@
+a = []
+a.append(input('возраст: '))
+a.append(input('пол:'))
+a.append(input('имя:'))
+a.append(input('город:'))
+a.append(input('класс:'))
+print(a)
