de090cbfa5571245ad4e8f066b7e17ecddbc1f8d
0irisch <isepeleva2045@gmail.com>
2026-01-26 19:28:40 +0000

    sdfghjkl

---
+++ task_2.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+
+x = np.arange(-10, 10, 6.02)
+y = 1 / (x + 0.05)
+plt.plot(x, y, label='my parabola')
+plt.savefig('Task2.png')
---
+++ task_4.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+b = 0.3
+phi = np.arange(0, 8*np.pi, 0.1)
+r = np.e ** (b * phi)
+
+
+x = r * np.cos(phi)
+y = r * np.sin(phi)
+
+
+plt.plot(x, y)
+plt.axis('equal')
+plt.savefig('Task_4.1.png')
+
+plt.close()
+k = 0.4
+
+phi = np.arange(0, 8*np.pi, 0.1)
+r = k * phi
+
+x = r * np.cos(phi)
+y - r * np.sin(phi)
+
+plt.plot(x, y)
+
+plt. axis('equal')
+plt.savefig('Task4_2.png')
+
+plt.close()
+alfa = 5
+phi = np.arange(0.01, 8*np.pi, 0.1)
+r = alfa / np.sqrt(phi)
+
+y = r * np.sin(phi)
+plt.plot(x, y)
+plt.axis('equal')
+plt.savefig('Task_4.3.png')
+
+plt.close()
+
+k = 8
+phi = np.arange(0, 8*np.pi, 0.1)
+r = np.sin(phi * k)
+
+x = r * np.cos(phi)
+y = r * np.sin(phi)
+
+
+
+plt.plot(x, y)
+plt.axis('equal')
+plt.savefig('Task_4.4.png')
